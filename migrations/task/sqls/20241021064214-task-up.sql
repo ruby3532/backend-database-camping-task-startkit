94b971b92a02c54349d81c633220bcdad21ea9fd
李心汝 <lixinru@MacBook-Air.local>
2024-12-01 17:49:08 +0800

    update task3-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -193,12 +193,7 @@ WHERE user_name IN ('Q太郎');
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 
--- ANS
-INSERT INTO "SKILL"(name)
-VALUES ('空中瑜伽');
 
-DELETE FROM "SKILL"
-WHERE name = '空中瑜伽';
 
 
 --  ████████  █████   █    █   █ 

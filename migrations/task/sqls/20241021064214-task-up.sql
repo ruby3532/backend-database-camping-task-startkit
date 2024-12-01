565c93ffce448abe0ba88b9007b0421632d976ce
李心汝 <lixinru@MacBook-Air.local>
2024-12-01 18:51:48 +0800

    update task3-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -186,11 +186,11 @@ VALUES (
 -- ANS
 UPDATE "COACH"
 SET experience_years = '3'
-WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io'));
+WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io');
 
 UPDATE "COACH"
 SET experience_years = '5'
-WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io'));
+WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io');
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
 
